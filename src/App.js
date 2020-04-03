a4b5f5510f07525e9dba73f5738ecda22f337c4a
bgv26 <bgv26@users.noreply.github.com>
2020-04-03 19:54:16 +0300

    Day 2 finish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const movie = {
   title: "Avengers. Infinity War",
   average_rate: 8.5,
   src: "https://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-  alt:
+  overview:
     "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos."
 };
 
--- src/MovieItem.js
+++ src/MovieItem.js
@@ -4,15 +4,49 @@ import "./styles.css";
 import Image from "./Image";
 
 class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+      like: false
+    };
+  }
+
+  toggleOverview = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  toggleLike = () => {
+    this.setState({
+      like: !this.state.like
+    });
+  };
+
   render() {
     const {
-      data: { title, average_rate, src }
+      data: { title, average_rate, src, overview }
     } = this.props;
     return (
       <div className="MovieItem">
         <Image src={src} alt={title} />
         <h1>{title}</h1>
         <h2>{average_rate}</h2>
+        <div className="ButtonBlock">
+          <button type="button" onClick={this.toggleOverview}>
+            {this.state.show ? "hide" : "show"}
+          </button>
+          <button
+            type="button"
+            onClick={this.toggleLike}
+            className={this.state.like ? "btn--like--checked" : ""}
+          >
+            {this.state.like ? "Unlike" : "Like"}
+          </button>
+        </div>
+        {this.state.show ? <p>{overview}</p> : null}
       </div>
     );
   }
