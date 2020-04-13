e24e71c293477deddec1f61cc874b2eaa19aa325
bgv26 <bgv26@mail.ru>
2020-04-13 21:00:59 +0300

    Updates

---
+++ precache-manifest.0f3dbdb53fab905ca8b7de2577f5b9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b29ba04461ad7cf73f3a",
+    "url": "/my-reactwarriors/static/css/main.2a025cf0.chunk.css"
+  },
+  {
+    "revision": "b29ba04461ad7cf73f3a",
+    "url": "/my-reactwarriors/static/js/main.76af1430.chunk.js"
+  },
+  {
+    "revision": "54ab6beb388c2d381471",
+    "url": "/my-reactwarriors/static/js/runtime~main.c39ed6bb.js"
+  },
+  {
+    "revision": "cf2c87c1208f89cc0e43",
+    "url": "/my-reactwarriors/static/css/2.6dda32b5.chunk.css"
+  },
+  {
+    "revision": "cf2c87c1208f89cc0e43",
+    "url": "/my-reactwarriors/static/js/2.290fb4a5.chunk.js"
+  },
+  {
+    "revision": "23dabde8c7e34ddba6b30eb7fcdaa962",
+    "url": "/my-reactwarriors/index.html"
+  }
+];
